2980018e4022a04b48c96bba8462f9cda609ee16
ahadjawaid <94938815+ahadjawaid@users.noreply.github.com>
2024-02-03 12:17:39 -0600

    feat: created forgot password page

---
+++ frontend/src/app/forgot-password/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+// Outlet
+import AuthOutlet from "@/outlets/AuthOutlet";
+
+// Components
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+
+//Constants
+import { paths } from "@/config";
+
+export default function Page() {
+    return (
+        <AuthOutlet>
+            <div className="flex flex-col gap-y-2">
+                <h2 className="text-lg font-semibold text-gray-900">
+                        Reset your password
+                </h2>
+                <p className="text-sm text-gray-700">
+                    Remember your password?&nbsp;
+                    <a href={paths.login} className="font-medium text-md text-indigo-600 
+                    hover:underline">
+                        Sign in&nbsp;
+                    </a>
+                    to your account.
+                </p>
+            </div>
+            <form onSubmit={() => {}} method="POST" className="flex flex-col gap-y-7">
+                <Input type="email" id="email" autoComplete="email" label="Email Address" required />
+                <Button type="submit" w-full>Send reset link</Button>
+                {/* {emailSent ? (
+                    <p className="bg-indigo-100 rounded-md px-3 py-2 text-center text-sm text-indigo-900">
+                        If an account with that email exists, we sent you an email with a link to reset your password.
+                    </p>
+                ) : null} */}
+            </form>
+        </AuthOutlet>
+    );
+}
