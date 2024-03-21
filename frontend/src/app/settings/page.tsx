39a5540ed571967820072913fbd13d8103b11619
Kelly Low <kelly.low404@gmail.com>
2024-03-21 18:19:00 -0500

    routed button and add place holder pages

---
+++ frontend/src/app/settings/page.tsx
@@ -0,0 +1,11 @@
+// ProfilePage.tsx
+
+const ProfilePage = () => {
+    return (
+        <div className="flex h-screen  bg-gray-100">
+            <h1 className="text-3xl font-bold text-gray-900">Settings</h1>
+        </div>
+    );
+};
+
+export default ProfilePage;
