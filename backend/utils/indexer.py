bf0ea2bc9d7b38c77bcb62b038190963a164ff5f
alexdivadi <dawork20100@gmail.com>
2024-04-10 14:09:57 -0500

    Switch to chroma client

--- backend/utils/indexer.py
+++ backend/utils/indexer.py
@@ -1,10 +1,7 @@
 import re
 
 from langchain.text_splitter import MarkdownHeaderTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_core.documents.base import Document
-
-embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
 def get_course(text: str) -> str:
     course = re.search(r'([A-Z][A-Z][A-Z]?[A-Z]?\s?\d[A-Z0-9]\d\d\.[a-zA-Z0-9][a-zA-Z0-9]\d)', text)
